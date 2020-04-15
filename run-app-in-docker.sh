3417c626cfbe75084142f0bf4ead072e5a0fe05f
Jack Sheridan <jackrileysheridan@gmail.com>
2020-04-14 17:28:07 -0700

    fixed proxy issue

--- run-app-in-docker.sh
+++ run-app-in-docker.sh
@@ -6,7 +6,4 @@ docker-compose kill
 # Forcefully removes any stopped service containers.
 docker-compose rm -f
 
-docker-compose build
-
-# Runs the app with port(s) enabled and mapped to the host. Removes the container after run.
-docker-compose run --rm --service-ports app
+docker-compose up
