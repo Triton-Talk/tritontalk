a5408cd0abf9a1a99f751404c1141c1c0ba0dfad
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-04-26 11:21:11 -0700

    fixed the build_react.sh script

--- build_react.sh
+++ build_react.sh
@@ -1,12 +1,13 @@
+echo "Removing old production build"
+rm -rf client/build testing/nginx/build
+
 echo "Creating a new production build"
 # create a production react build
 npm run --silent --prefix client/ build > /dev/null
-# set up production react build to be served by nginx
-cp -r client/build ./build
 
 echo "press y to copy the new react build to the server"
 read -n1 answer
 
 if [[ $answer == 'y' ]]; then
-    scp -r ./build jack@tritontalk.com:~/build_$USER
+    scp -r ./client/build jack@tritontalk.com:~/build_$USER
 fi
