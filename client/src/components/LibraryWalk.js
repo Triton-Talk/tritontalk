e3ca9f486c3aec8734f9f0b27dd9ee9a8a9af2b1
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-04-30 15:46:47 -0700

    it compiles butthere's still errors

--- client/src/components/LibraryWalk.js
+++ client/src/components/LibraryWalk.js
@@ -4,57 +4,57 @@ import PropTypes from 'prop-types'
 class LibraryWalk extends Component {
 
   render() {
+    var config = {
+            type: window.Phaser.AUTO,
+            width: 800,
+            height: 600,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 200 }
+                }
+            },
+            scene: {
+                preload: preload,
+                create: create
+            }
+        };
+
+        var game = new window.Phaser.Game(config);
+
+        function preload ()
+        {
+            this.load.setBaseURL('http://labs.phaser.io');
+
+            this.load.image('sky', 'assets/skies/space3.png');
+            this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+            this.load.image('red', 'assets/particles/red.png');
+        }
+
+        function create ()
+        {
+            this.add.image(400, 300, 'sky');
+
+            var particles = this.add.particles('red');
+
+            var emitter = particles.createEmitter({
+                speed: 100,
+                scale: { start: 1, end: 0 },
+                blendMode: 'ADD'
+            });
+
+            var logo = this.physics.add.image(400, 100, 'logo');
+
+            logo.setVelocity(100, 200);
+            logo.setBounce(1, 1);
+            logo.setCollideWorldBounds(true);
+
+            emitter.startFollow(logo);
+        }
     return (
-	var config = {
-		type: window.Phaser.AUTO,
-		width: 800,
-		height: 600,
-		physics: {
-		    default: 'arcade',
-		    arcade: {
-			gravity: { y: 200 }
-		    }
-		},
-		scene: {
-		    preload: preload,
-		    create: create
-		}
-	    };
-
-	    var game = new window.Phaser.Game(config);
-
-	    function preload ()
-	    {
-		this.load.setBaseURL('http://labs.phaser.io');
-
-		this.load.image('sky', 'assets/skies/space3.png');
-		this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-		this.load.image('red', 'assets/particles/red.png');
-	    }
-
-	    function create ()
-	    {
-		this.add.image(400, 300, 'sky');
-
-		var particles = this.add.particles('red');
-
-		var emitter = particles.createEmitter({
-		    speed: 100,
-		    scale: { start: 1, end: 0 },
-		    blendMode: 'ADD'
-		});
-
-		var logo = this.physics.add.image(400, 100, 'logo');
-
-		logo.setVelocity(100, 200);
-		logo.setBounce(1, 1);
-		logo.setCollideWorldBounds(true);
-
-		emitter.startFollow(logo);
-	    }
-		
+      <p> phaser goes here </p>		
     )
   }
 }
 
-export default ChatInput
+export default LibraryWalk 
