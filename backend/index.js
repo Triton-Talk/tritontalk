5c8e5a9633613d7e1fae9afe077474792f111c26
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-07 12:26:19 -0700

    restructred backend firebase location

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const admin = require('./firebase')
+const admin = require('./firebase/firebase')
 
 const userRouter = require('./routers/user')
 const videoRouter = require('./routers/video')
