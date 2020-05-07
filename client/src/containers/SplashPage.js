044be34bb52da6e4d745af11d82effac8d91bd40
Michael Askndafi <maskndafi@gmail.com>
2020-05-06 22:52:06 -0700

    Splash

--- client/src/containers/SplashPage.js
+++ client/src/containers/SplashPage.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import styles from '../styles/SplashPage.css';
 import BackgroundImage from "./SplashPageBackground.png";
+import SplashPageBackground from './SplashPageBackground.png'
+
+import Auth from '../context/auth';
+import Page from '../context/page';
 
 function SplashPage() {
+  const { user, handleSignOn, handleSignOut } = useContext(Auth);
+  const { page, setPage } = useContext(Page)
   return (
-    <div styles={{ ...styles, backgroundImage: { BackgroundImage } }}>
+    <div className="App" style={{backgroundImage: `url(${SplashPageBackground})` }}>
       <h1 className="Logo">TritonTalk</h1>
       <h3 className="SubText">The Virtual Library Walk</h3>
+      <div className="vertical">
+        <button className="button">SIGN UP</button>
+        <button className="button" onClick={handleSignOn}>LOGIN</button>
+      </div>
     </div>
   );
 }
