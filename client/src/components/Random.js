fef6352fc3ce0dfc5116c240db47f38d29753ad7
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-06 22:29:56 -0700

    styled random a little better

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -38,37 +38,26 @@ const Random = (props) => {
 
         </Form.Row>
 
-        <Form.Group controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
 
 
         {/* Address */}
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
           <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
+            <Form.Label>College</Form.Label>
             <Form.Control as="select" value="Choose...">
               <option>Choose...</option>
               <option>...</option>
             </Form.Control>
           </Form.Group>
 
-
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Major</Form.Label>
+            <Form.Control as="select" value="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Control>
+          </Form.Group>
         </Form.Row>
-
-
-
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="switch" id="custom-switch"
-            label="Make my Organization Public to Library Walk" />
-
-        </Form.Group>
         <center>
           <hr></hr>
           <Button variant="dark" type="submit">
