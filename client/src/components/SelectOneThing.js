08269e28beb318228d59e27ed53017e477bb84e0
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-07 15:13:36 -0700

    more mods to clubs for setttings

--- client/src/components/SelectOneThing.js
+++ client/src/components/SelectOneThing.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
-const SelectOneThing = ({label, options, value, onChange}) => {
-
+const SelectOneThing = ({ label, options, value, onChange }) => {
+  const hi = (label == 'Clubs') ? (<Form.Label>{label} <Button className="btn-warning">Add More Clubs</Button></Form.Label>) : (<Form.Label>{label} </Form.Label>)
   return (
     <Form.Group>
-      <Form.Label>{label}</Form.Label>
+
+      {hi}
       <Form.Control size="lg" as="select" value={value} onChange={onChange}>
-        { options.map((e, i) => <option key={i}>{e}</option>) }
+        {options.map((e, i) => <option key={i}>{e}</option>)}
       </Form.Control>
     </Form.Group>
   );
