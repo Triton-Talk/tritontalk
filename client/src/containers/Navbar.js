3aabed3df8f9dc8b9a6e07c3ab1403198149131b
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-08 01:37:55 -0700

    fixed nav bar how it would go down

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {Navbar} from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/NavBar.css';
 
@@ -16,32 +16,32 @@ const NavigationBar = () => {
       <Navbar style={styles} bg="dark" variant="dark">
         <Navbar.Toggle />
         {
-          user !== null && user !== undefined ?  
-          <> 
-            <Link to='/random'><button className='navbtn'>Go Chat</button></Link>
-            <div className="centered">
-              <Link to="/"><h1 className="NavBarLogo">TritonTalk</h1></Link>
-            </div>
-            <Navbar.Collapse className="justify-content-end">
-              <div className="dropdown">
-                <button className="navbtn">Account</button>
-                <div className="dropdown-content">
-                  <Link to='/profile'>
-                    <button className="dropdown">Settings</button>
-                  </Link>
+          user !== null && user !== undefined ?
+            <>
+              <Link to='/random'><button className='navbtn'>Go Chat</button></Link>
+              <div className="centered" style={{ textAlign: "center", height: "10px" }}>
+                <Link to="/"><h1 className="NavBarLogo">TritonTalk</h1></Link>
+              </div>
+              <Navbar.Collapse className="justify-content-end">
+                <div className="dropdown">
+                  <button className="navbtn">Account</button>
+                  <div className="dropdown-content">
+                    <Link to='/profile'>
+                      <button className="dropdown">Settings</button>
+                    </Link>
 
-                  <button className="dropdown" 
-                          onClick={handleSignOut}>Sign out</button>
+                    <button className="dropdown"
+                      onClick={handleSignOut}>Sign out</button>
+                  </div>
                 </div>
-              </div> 
-            </Navbar.Collapse>
-          </>
-          :
-          <div className="centered">
-            <h1 className="NavBarLogo">TritonTalk</h1>
-          </div>
+              </Navbar.Collapse>
+            </>
+            :
+            <div className="centered">
+              <h1 className="NavBarLogo">TritonTalk</h1>
+            </div>
         }
-      </Navbar > 
+      </Navbar >
     </div>
   )
 }
