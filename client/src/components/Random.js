30d65d00e8a664cdd13c343960e79e2654d00d2c
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-08 02:26:38 -0700

    repeating image

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -7,7 +7,7 @@ const Random = (props) => {
       <br></br>
       <div style={{ textAlign: "center" }}>
         <h1>Who would you like to meet today?</h1>
-        <Jumbotron fluid style={{ marginRight: "5%", marginLeft: "5%", backgroundImage: 'url(https://ucsdnews.ucsd.edu/news_uploads/Back-to-School-150110.jpg', backgroundPosition: "0px -170px", height: "200px", backgroundRepeat: 'no-repeat' }}></Jumbotron>
+        <Jumbotron fluid style={{ marginRight: "5%", marginLeft: "5%", backgroundImage: 'url(https://ucsdnews.ucsd.edu/news_uploads/Back-to-School-150110.jpg', backgroundPosition: "0px -170px", height: "200px", backgroundRepeat: 'repeat' }}></Jumbotron>
       </div>
       <Form style={{ maxWidth: "95%" }}>
 
