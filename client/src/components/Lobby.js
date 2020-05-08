431b803d4361286b244fbc9e2d9dabc145531854
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-08 02:51:37 -0700

    repeating image

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,5 +1,6 @@
 import Auth from '../context/auth'
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 const Lobby = ({
   roomName,
@@ -9,29 +10,36 @@ const Lobby = ({
   const { user } = React.useContext(Auth)
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Enter a room</h2>
-      <div>
-        {user ?
-          <label htmlFor="name">Name: {user.name} </label> :
-          <label htmlFor="name">Name: Please sign in first!</label>}
-      </div>
+    <div>
+      <center>
+        <Card style={{ width: "20%", marginBottom: "10px", paddingBottom: "10px", margin: "50px" }}>
+          <Card.Header style={{ marginBottom: "10px" }}>For Dev Purposes</Card.Header>
+          <form onSubmit={handleSubmit}>
+            <h2>Enter a room</h2>
+            <div>
+              {user ?
+                <label htmlFor="name">Name: {user.name} </label> :
+                <label htmlFor="name">Name: Please sign in first!</label>}
+            </div>
 
-      <div>
-        <label htmlFor="room">Room name:</label>
-        <input
-          type="text"
-          id="room"
-          value={roomName}
-          onChange={handleRoomNameChange}
-          required
-        /><br></br>
-        <button style={{ marginRight: "10px" }} type="submit">Submit</button>
-        <Link to='/newclub'>
-          <button className="btn-success">Register a Club</button>
-        </Link>
-      </div>
-    </form>
+            <div>
+              <label htmlFor="room">Room name:</label>
+              <input
+                type="text"
+                id="room"
+                value={roomName}
+                onChange={handleRoomNameChange}
+                required
+              /><br></br>
+              <button style={{ marginRight: "10px" }} type="submit">Submit</button>
+              <Link to='/newclub'>
+                <button className="btn-success">Register a Club</button>
+              </Link>
+            </div>
+          </form>
+        </Card>
+      </center>
+    </div>
   );
 };
 
