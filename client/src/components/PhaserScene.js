6a61123d34d480b0b66f4a9ad7bfd3691fe50ab4
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-09 04:27:04 -0700

    change ur character yeet

--- client/src/components/PhaserScene.js
+++ client/src/components/PhaserScene.js
@@ -13,21 +13,33 @@ class PhaserScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
+    this.load.setBaseURL('https://labs.phaser.io');
     this.load.image('player', 'assets/sprites/ghost.png');
+    this.load.image('bunny', 'assets/sprites/bunny.png');
   }
 
   create() {
-    this.player = this.physics.add.image(400, 300, 'player');
+    this.player = this.physics.add.sprite(400, 300, 'player');
+    this.player.setCollideWorldBounds(true);
+
+    this.player.setInteractive()
+    this.clickCount = 0
+    this.input.on('gameobjectdown', console.log) 
+
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.player.setCollideWorldBounds(true);
+
+    this.key1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+    this.key2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
+
     this.upKeyDebug = this.add.text(300, 10, 'Up', { font: '16px Courier', fill: '#00ff00' });
+
     this.socket = io(URL);
     this.playerData = null
     this.players = {}
+
 
     //set up a callback that logs new data (for now)
     this.socket.on('update-players', (data) => {
@@ -38,7 +50,8 @@ class PhaserScene extends Phaser.Scene {
           (this.keyS.isDown ? 'Down ' : '') + 
           (this.keyD.isDown ? 'Right ' : ''),
           'position: ' + this.player.x + ',' + this.player.y,
-          'position from socket.io: ' + data[this.socket.id].x + ',' + data[this.socket.id].y
+          'position from socket.io: ' + data[this.socket.id].x + ',' + data[this.socket.id].y, 
+          'click count: ' + this.clickCount
       ]);
 
       console.log('new player data received', data)
@@ -89,6 +102,12 @@ class PhaserScene extends Phaser.Scene {
           playerName: this.socket.id
         });
       }
+
+      if(this.key1.isDown)
+        this.player.setTexture('player')
+
+      if(this.key2.isDown)
+        this.player.setTexture('bunny')
 
       for(let player in this.playerData){
         if(player === this.socket.id)
