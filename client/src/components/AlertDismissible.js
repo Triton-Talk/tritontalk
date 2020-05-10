ae953711221ef768c93f6daf963fc1178429090c
Mike Kaufman <mikekaufman4@gmail.com>
2020-05-09 19:20:59 -0700

    added link to update

--- client/src/components/AlertDismissible.js
+++ client/src/components/AlertDismissible.js
@@ -9,7 +9,7 @@ export default function AlertDismissible() {
       <Alert show={show} style={{ backgroundColor: "rgb(190,147,54)" }}>
         <Alert.Heading><Info style={{ marginRight: "7px", marginBottom: "4px" }}></Info>COVID-19 Updates</Alert.Heading>
         <p>
-          Visit UC San Diego’s Coronavirus portal for the latest information for the campus community.
+          Visit UC San Diego’s Coronavirus portal for the latest information for the campus community. <a style={{ color: "black", textDecorationLine: 'underline' }} href="https://coronavirus.ucsd.edu/?_ga=2.263315855.2087258241.1588918279-399187423.1541192885">VIEW DETAILS</a>
         </p>
         <hr />
         <div className="d-flex justify-content-center">
