cb4f65d66eae1d3208d2b38168488de4f0b35db4
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-10 02:06:53 -0700

    users can clear alert permanetly

--- client/src/components/AlertDismissible.js
+++ client/src/components/AlertDismissible.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Alert, Button } from 'react-bootstrap';
 import { Info } from '@material-ui/icons';
-export default function AlertDismissible() {
+
+
+export default function AlertDismissible(props) {
   const [show, setShow] = useState(true);
 
   return (
@@ -9,12 +11,22 @@ export default function AlertDismissible() {
       <Alert show={show} style={{ backgroundColor: "rgb(190,147,54)" }}>
         <Alert.Heading><Info style={{ marginRight: "7px", marginBottom: "4px" }}></Info>COVID-19 Updates</Alert.Heading>
         <p>
-          Visit UC San Diego’s Coronavirus portal for the latest information for the campus community. <a style={{ color: "black", textDecorationLine: 'underline' }} href="https://coronavirus.ucsd.edu/?_ga=2.263315855.2087258241.1588918279-399187423.1541192885">VIEW DETAILS</a>
+          Visit UC San Diego’s Coronavirus portal for the latest information for the campus community. 
+          <a style={{ color: "black", textDecorationLine: 'underline' }} 
+             href="https://coronavirus.ucsd.edu/?_ga=2.263315855.2087258241.1588918279-399187423.1541192885">VIEW DETAILS</a>
         </p>
         <hr />
         <div className="d-flex justify-content-center">
           <Button onClick={() => setShow(false)} variant="outline-dark">
             Close
+          </Button>
+
+          <Button style={{margin: '0px 2vw'}} onClick={() => {
+            setShow(false)
+            props.killAlert()   
+          }}
+                  variant="outline-dark">
+            Hide Permanently 
           </Button>
         </div>
       </Alert>
