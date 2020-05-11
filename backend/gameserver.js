d424aac74f7e2e84f4a30d8b03468c2000b1188a
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-10 23:24:38 -0700

    move game socket into different file

---
+++ backend/gameserver.js
@@ -0,0 +1,52 @@
+const startGameServer = httpServer => {
+  const phaser = require('socket.io')(httpServer);
+
+  const players = {}
+  const oldPlayers = {}
+
+  phaser.on('connection', socket => {
+
+    console.log('new connection to phaser socket')
+
+    // When a player connects
+    socket.on('new-player', state => {
+      console.log('New player joined with state:', state)
+      players[socket.id] = state
+
+      // Emit the update-players method in the client side
+      socket.emit('update-players', players)
+
+    })
+
+    socket.on('disconnect', state => {
+      delete players[socket.id]
+      phaser.emit('update-players', players)
+    })
+
+    // When a player moves
+    socket.on('move-player', data => {
+      const {x, y, playerName} = data
+
+      // If the player is invalid, return
+      if (players[socket.id] === undefined) {
+        return
+      }
+
+      // Update the player's data if he moved
+      players[socket.id].x = x
+      players[socket.id].y = y
+
+      players[socket.id].playerName = playerName
+
+      // Send the data back to the client
+      phaser.emit('update-players', players)
+    })
+    
+    const updatePlayers = () => {
+      phaser.emit('update-players', players)
+      setTimeout(() => updatePlayers(), 17)
+    }
+  })
+}
+
+module.exports = {start: startGameServer}
--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ const authMiddleware = require('./routers/auth')
 const userRouter = require('./routers/user')
 const videoRouter = require('./routers/video')
 
+const gameserver = require('./gameserver')
+
 const { videoToken } = require('./video/tokens');
 const config = require('./video/config');
 
@@ -37,63 +39,6 @@ app.use(userRouter)
 
 server = app.listen(3001, () => console.log('node running on localhost:3001'));
 
-/*
-const chat = require('socket.io')(server);
-chat.on('connection', (socket) => {
-  socket.on('new_message', (data) => {
-    console.log(data)
-    socket.emit('new_message', data);
-  })
-})
-*/
-
-const phaser = require('socket.io')(server);
-
-const players = {}
-const oldPlayers = {}
-
-phaser.on('connection', socket => {
-
-  console.log('new connection to phaser socket')
-
-  // When a player connects
-  socket.on('new-player', state => {
-    console.log('New player joined with state:', state)
-    players[socket.id] = state
-
-    // Emit the update-players method in the client side
-    socket.emit('update-players', players)
-
-  })
-
-  socket.on('disconnect', state => {
-    delete players[socket.id]
-    phaser.emit('update-players', players)
-  })
-
-  // When a player moves
-  socket.on('move-player', data => {
-    const {x, y, playerName} = data
-
-    // If the player is invalid, return
-    if (players[socket.id] === undefined) {
-      return
-    }
-
-    // Update the player's data if he moved
-    players[socket.id].x = x
-    players[socket.id].y = y
-
-    players[socket.id].playerName = playerName
-
-    // Send the data back to the client
-    phaser.emit('update-players', players)
-  })
-  
-  const updatePlayers = () => {
-    phaser.emit('update-players', players)
-    setTimeout(() => updatePlayers(), 17)
-  }
-})
+gameserver.start(server)
 
 module.exports = app
