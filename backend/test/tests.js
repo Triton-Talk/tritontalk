4169e0584082462df9cb6b10b6d1309d34468b9c
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-13 01:14:01 -0700

    database testing for all objects

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -29,6 +29,7 @@ describe('Testing basic HTTP server functionality', function(){
 
 })
 
+/*
 describe('Testing auth.js', function(){
 
   before(async function(){
@@ -80,3 +81,148 @@ describe('Testing auth.js', function(){
   })
 
 })
+*/
+
+describe('Database testing', function(done){
+
+  describe('User tests', function(done){
+
+    it('creates a new User', function(done){
+      const User = require('../models/User')
+      
+      const exampleuser = new User({
+        name: 'Example user',
+        email: 'example@ucsd.edu', 
+      })
+
+      exampleuser.save().then(() => done())
+    })
+
+    it('reads a User', function(done){
+      const User = require('../models/User')
+      
+      const query = {email: 'example@ucsd.edu'}
+
+      User.findOne(query).then(exampleuser => {
+        chai.expect(exampleuser.name).to.equal('Example user')
+
+        done()
+      })
+    })
+
+    it('updates a User', function(done){
+      const User = require('../models/User')
+      
+      const query = {email: 'example@ucsd.edu'}
+
+      User.findOneAndUpdate(query, {bio: 'example update'}, {new: true}).then(exampleuser => {
+        chai.expect(exampleuser.name).to.equal('Example user')
+        chai.expect(exampleuser.bio).to.equal('example update')
+
+        done()
+      })
+    })
+
+    it('deletes a User', async function(){
+      const User = require('../models/User')
+      
+      const query = {email: 'example@ucsd.edu'}
+
+      const exampleuser = await User.findOne(query)
+      
+      exampleuser.remove()
+
+      const e = await User.findOne(query) 
+      chai.expect(e).to.be.null
+    })
+
+  })
+
+  describe('Club tests', function(done){
+
+    it('creates a new Club', function(done){
+      const Club = require('../models/Club')
+
+      const exampleclub = new Club({
+        name: 'Example club',
+        description: 'example club'
+      })
+
+      exampleclub.save().then(() => done())
+    })
+
+    it('reads a Club', function(done){
+      const Club = require('../models/Club')
+
+      const query = { name: 'Example club' }
+
+      Club.findOne(query).then(exampleclub => {
+        chai.expect(exampleclub.description).to.equal('example club')
+        
+        done()
+      })
+    })
+
+    it('updates a Club', function(done){
+      const Club = require('../models/Club')
+
+      const query = { name: 'Example club' }
+
+      Club.findOneAndUpdate(query, {description: 'new description'}, {new: true}).then(exampleclub => {
+        chai.expect(exampleclub.description).to.equal('new description')
+        
+        done()
+      })
+    })
+
+    it('deletes a Club', async function(){
+      const Club = require('../models/Club')
+
+      const query = { name: 'Example club' }
+
+      const exampleclub = await Club.findOne(query)
+
+      exampleclub.remove()
+
+      const c = await Club.findOne(query)
+      chai.expect(c).to.be.null
+    })
+
+  })
+
+/*
+
+  const shubham = new User({
+    name: 'Shubham', 
+    email: 'skulkarn@ucsd.edu',
+    bio: 'I am a second year computer science major',
+    hobbies: 'coding' 
+  })
+
+
+  const Room = require('./Room')
+
+  const gbm = new Room({
+    name: 'SCSC General Body Meeting'
+  })
+
+  shubham.clubs.push(scsc)
+
+  shubham.hosted_rooms.push(gbm)
+  gbm.authorized_users.push(shubham)
+
+  gbm.chat_logs.push({
+    message: 'hey everybody!',
+    sent_by_user: shubham
+  })
+
+  console.log('user', shubham, '\n')
+  console.log('club', scsc, '\n')
+  console.log('room', gbm, '\n')
+
+  shubham.save()
+  scsc.save()
+  gbm.save()
+  */
+})
+
