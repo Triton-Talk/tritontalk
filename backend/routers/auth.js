8da766a3c634fa4957f7a3888aed634ee4cfce69
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-13 04:25:10 -0700

    fixed auth.js to not have any unneeded errors on invalid token

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -5,49 +5,50 @@ const admin = require('../utils/firebase')
 const router = new express.Router();
 
 // PREPROCESSOR FOR ALL API ROUTES
-router.use('/api/*', (req, res, next) => {
+router.use('/api/*', async (req, res, next) => {
   if(req.cookies.sessionCookie){
-    const sessionCookie = req.cookies.sessionCookie || '';
+    try{
+      const sessionCookie = req.cookies.sessionCookie || '';
 
-    admin.auth().verifySessionCookie(sessionCookie).then(identity => {
+      const identity = await admin.auth().verifySessionCookie(sessionCookie)
       req.identity = identity
 
       console.log('Identity processed via cookie, continuing.')
       next()
-    }).catch(error => {
+    }
+    catch(error){
       console.log(`Failed to verify identity by cookie: ${req.cookies}`);
       res.clearCookie('sessionCookie')
-      res.status(404).send('Error: verify identity by cookie')
-    });
+      return res.status(404).send('Error: verify identity by cookie')
+    }
   }
   else {
     //verify identity
     try{
-      admin.auth().verifyIdToken(req.body.credential).then(identity => {
-        req.identity = identity
-        //verify ucsd email address
-        if(identity.email.substr(identity.email.lastIndexOf('@')) !== '@ucsd.edu')
-          throw new Error();
+      const identity = await admin.auth().verifyIdToken(req.body.credential)
+      req.identity = identity
 
-        console.log('Identity processed via request body')
+      //verify ucsd email address
+      if(identity.email.substr(identity.email.lastIndexOf('@')) !== '@ucsd.edu')
+        throw "invalid email address. Please try UCSD email"
 
-        const expiresIn = 60 * 60 * 24 * 1000;
+      console.log('Identity processed via request body')
 
-        return admin.auth().createSessionCookie(req.body.credential, {expiresIn})
-      }).then(cookie => {
-        //set a session cookie
-        //const options = {maxAge: expiresIn, httpOnly: true, secure: true};
-        const options = {}
-        res.cookie('sessionCookie', cookie, options)
+      const expiresIn = 60 * 60 * 24 * 1000 * 14;
 
-        console.log('Setting session cookie')
+      const cookie = await admin.auth().createSessionCookie(req.body.credential, {expiresIn})
 
-        next()
-      })
+      //set a session cookie
+      const options = {maxAge: expiresIn, httpOnly: true, secure: true};
+      res.cookie('sessionCookie', cookie, options)
+
+      console.log('Setting session cookie')
+
+      next()
     }
     catch(error){
       console.log(`Failed to parse identity: ${req.body}`);
-      res.status(404).send('Error: failed to parse identity')
+      return res.status(404).send('Error: failed to parse identity')
     }
   }
 })
