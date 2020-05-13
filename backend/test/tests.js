62c89ec2d46415fd13afa4c96ad68fbf378b25c6
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-13 03:55:22 -0700

    cleaning up the file

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -329,39 +329,4 @@ describe('Basic database testing - CRUD functionality', function(done){
 
   })
 
-/*
-
-  const shubham = new User({
-    name: 'Shubham',
-    email: 'skulkarn@ucsd.edu',
-    bio: 'I am a second year computer science major',
-    hobbies: 'coding'
-  })
-
-
-  const Room = require('./Room')
-
-  const gbm = new Room({
-    name: 'SCSC General Body Meeting'
-  })
-
-  shubham.clubs.push(scsc)
-
-  shubham.hosted_rooms.push(gbm)
-  gbm.authorized_users.push(shubham)
-
-  gbm.chat_logs.push({
-    message: 'hey everybody!',
-    sent_by_user: shubham
-  })
-
-  console.log('user', shubham, '\n')
-  console.log('club', scsc, '\n')
-  console.log('room', gbm, '\n')
-
-  shubham.save()
-  scsc.save()
-  gbm.save()
-  */
 })
-
