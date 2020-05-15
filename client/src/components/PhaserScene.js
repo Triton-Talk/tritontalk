d665450f586ed3473d3c4e6ec67d0be46237d219
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-15 03:18:37 -0700

    fix pls

--- client/src/components/PhaserScene.js
+++ client/src/components/PhaserScene.js
@@ -174,7 +174,7 @@ class PhaserScene extends Phaser.Scene {
 
     this.upKeyDebug = this.add.text(300, 10, 'Up', { font: '16px Courier', fill: '#00ff00' });
 
-    this.socket = io(URL, {transport: ['websocket'], secure: false})
+    this.socket = io(URL, {transport: ['websocket']})
     this.playerData = null
     this.player_updates = []
     this.players = {}
