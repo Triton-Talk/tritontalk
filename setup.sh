f90cc0f7220280f391990b0fced8aaa373114805
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-05-18 11:34:20 -0700

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ npm --prefix backend/ install
 npm --prefix client/ install
 
 scp jack@tritontalk.com:~/.env backend/.env
+scp jack@tritontalk.com:~/tritontalk-d063d-a82351d39ace.json backend/utils/tritontalk-d063d-a82351d39ace.json
 
 echo "To run: do npm run dev in both client/ and backend/ folders (in 2 different terminals)"
